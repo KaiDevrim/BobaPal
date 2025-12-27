1e89d3836bf196d078329f4c6c68a65535483b8e
Kai Devrim <kai@devrim.tech>
2025-12-28 00:42:06 +0300

    Setup storage and update Drink model

--- database/model/Drink.ts
+++ database/model/Drink.ts
@@ -1,21 +1,18 @@
-import { Model } from '@nozbe/watermelondb'
-import { date, field, text } from '@nozbe/watermelondb/decorators';
+import { Model } from '@nozbe/watermelondb';
+import { field, text, date } from '@nozbe/watermelondb/decorators';
 
 export default class Drink extends Model {
   static table = 'drinks';
 
-  // @ts-ignore
-  @text('flavor') flavor: string;
-  // @ts-ignore
-  @text('photo_url') photoUrl: string;
-  // @ts-ignore
-  @field('rating') rating: number;
-  // @ts-ignore
-  @field('price') price: number;
-  // @ts-ignore
-  @text('occasion') occasion: string;
-  // @ts-ignore
-  @text('store') store: string;
-  // @ts-ignore
-  @date('date') date: string;
+  @text('flavor') flavor!: string;
+  @field('price') price!: number;
+  @text('store') store!: string;
+  @text('occasion') occasion!: string;
+  @field('rating') rating!: number;
+  @text('date') date!: string;
+  @text('photo_url') photoUrl!: string | null;
+  @text('s3_key') s3Key!: string | null;
+  @text('user_id') userId!: string;
+  @field('synced') synced!: boolean;
+  @date('last_modified') lastModified!: Date;
 }
